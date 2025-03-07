db6da00e0b77190e01ba884bed53c1f44342512a
salas <salas@trajano.us.es>
2025-03-07 09:25:34 +0100

    tmy0909

--- lib/src/main/java/us/dit/Control.java
+++ lib/src/main/java/us/dit/Control.java
@@ -1,1 +1,23 @@
+//uvus:tmy0909
+//pass:1741335195
 
+package us.dit;
+
+public class Control {
+
+    public static void main(String[] args) {
+        int numero = 5; 
+        int resultado = factorial(numero);
+
+        System.out.println("El factorial de " + numero + " es: " + resultado);
+    }
+
+    // Calcular el factorial de un número
+    public static int factorial(int num) {
+        if (num == 0) {
+            return 1; // El factorial de 0 es 1
+        } else {
+            return num * factorial(num - 1);
+        }
+    }
+}
